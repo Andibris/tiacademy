0565249758860260db8860a781804f9a30ef73c5
anderson brischiliari <anderson_brischiliari@hotmail.com>
2021-08-29 17:37:45 -0300

    ciclo 3, aula 3, desafio

--- CICLO3/servicesti/Controller.js
+++ CICLO3/servicesti/Controller.js
@@ -15,14 +15,10 @@ app.get('/', function(req,res){
     res.send('Olá Mundo!');
 });
 
-app.get('/clientes', async(req,res)=>{
-    let create=await cliente.create({
-        nome: "Anderson Breschigliaro",
-        endereco: "Rua dois, 150",
-        cidade: "Maringá",
-        uf: "PR",
-        nascimento: "1989-12-13",
-    });    
+app.post('/clientes', async(req,res)=>{
+    let create=await cliente.create(
+        req.body
+    );    
     res.send('Novo cliente cadastrado!');
 });
 
@@ -33,13 +29,10 @@ app.post('/servicos', async(req,res)=>{
     res.send('Serviço foi inserido!');
 });
 
-app.get('/pedidos', async(req,res)=>{
-    let create=await pedido.create({
-        idCliente: "1",
-        idServico: "1",
-        valor: "550",
-        data: "2021-08-27",
-    });
+app.post('/pedidos', async(req,res)=>{
+    let create=await pedido.create(
+        req.body
+    );
     res.send('Novo pedido criado!');
 });
 
