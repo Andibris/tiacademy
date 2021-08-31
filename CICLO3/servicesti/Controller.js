fabdbb8b72a41115abca30a7ce7773f759706cdb
anderson brischiliari <anderson_brischiliari@hotmail.com>
2021-08-31 18:03:05 -0300

    ciclo3aula5+desafio

--- CICLO3/servicesti/Controller.js
+++ CICLO3/servicesti/Controller.js
@@ -128,6 +128,147 @@ app.get('/somapedidos/:id',async(req,res)=>{
     });
 });
 
+app.get('/atualizaservico', async(req,res)=>{
+    await servico.findByPk(1)
+    .then(servico =>{
+        servico.nome='HTML/CSS/JS';
+        servico.descricao='Páginas estáticas e dinâmicas estilizadas';
+        servico.save();
+        return res.json({servico});
+    });
+});
+
+app.put('/editarservico', (req,res)=>{
+    servico.update(req.body,{
+        where: {id: req.body.id}
+    }).then(function(){
+        return res.json({
+            error: false,
+            message: "Serviço alterado com sucesso."
+        });
+    }).catch(function(erro){
+        return res.status(400).json({
+            error: true,
+            message: "Erro na alteração do serviço."
+        });
+    });
+});
+
+app.get('/servicospedidos', async(req,res)=>{
+    await servico.findByPk(1, {
+        include: [{all:true}]
+    }).then(servico =>{
+        return res.json({servico});
+    });
+});
+
+app.put('/editarpedido', (req,res)=>{
+    pedido.update(req.body,{
+        where: {ServicoId: req.body.ServicoId}
+    }).then(function(){
+        return res.json({
+            error: false,
+            message: "Pedido alterado com sucesso."
+        });
+    }).catch(function(erro){
+        return res.status(400).json({
+            error: true,
+            message: "Erro na alteração do pedido."
+        });
+    });
+});
+
+//buscar serviço de cliente passando o id do cliente no corpo da req
+app.get('/servicosclientes', async (req,res)=>{
+    await cliente.findByPk(req.body.id, {
+        include: [{all: true}],
+    }).then(cliente =>{
+        return res.json({cliente});
+    });
+});
+
+//usar rota para consultar cliente e edita-lo pelo metodo put
+app.put('/editarcliente/:id', (req,res)=>{
+    cliente.update(req.body,{
+        where: {id: req.params.id}
+    }).then(function(){
+        return res.json({
+            error: false,
+            message: "Cliente alterado com sucesso."
+        });
+    }).catch(function(erro){
+        return res.status(400).json({
+            error: true,
+            message: "Erro na alteração do cliente."
+        });
+    });
+});
+
+//usar rota para consultar pedidos e edita-lo pelo metodo put
+app.put('/editarpedido/:id', (req,res)=>{
+    pedido.update(req.body,{
+        where: {id: req.params.id}
+    }).then(function(){
+        return res.json({
+            error: false,
+            message: "Pedido alterado com sucesso."
+        });
+    }).catch(function(erro){
+        return res.status(400).json({
+            error: true,
+            message: "Erro na alteração do pedido."
+        });
+    });
+});
+
+app.get('/excluircliente', async(req,res)=>{
+    cliente.destroy({
+        where: {id: 5}
+    });
+});
+
+app.delete('/apagarcliente/:id', (req,res)=>{
+    cliente.destroy({
+        where: {id: req.params.id}
+    }).then(function(){
+        return res.json({
+        error: false,
+        message: "Cliente excluido."
+    });
+}).catch(function(erro){
+    return res.status(400).json({
+        error: true,
+        message: "Não foi possível excluir o cliente."
+        });
+    });
+});
+
+//desafios aula 4
+app.get('/pedidoscliente/:id', async (req, res)=>{
+    await pedido.findAll({ where: { ClienteId: [req.params.id]  } })
+    .then(function(pedidos){
+        res.json(pedidos)
+    });
+    console.log(pedidos,valor,ClienteId)
+    });
+    
+//desafio aula 4
+app.put('/editarpedidocliente/:id',(req,res)=>{
+    pedido.update(req.body,{
+            where: {id: req.params.id}
+        }).then(function(){
+            return res.json({
+                error: false,
+                message: "Pedido alterado com sucesso."
+            });
+        }).catch(function(erro){
+            return res.status(400).json({
+                error: true,
+                message: "Não foi possível editar o pedido."
+        });
+    });
+});
+
 let port=process.env.PORT || 3000;
 
 app.listen(port,(req,res)=>{
